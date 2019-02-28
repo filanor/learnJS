f805e3678130f6a4b98c4304c0be92e5031ab3a3
filanor <filanor@gmail.com>
2019-02-28 22:59:35 +0300

    Первая версия ДЗ по первому уроку (база)

---
+++ lesson 1/js/script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// ОБъявляем переменные (п.1)
+var money,
+    time;
+
+//Запрашиваем пользователя данные (п.2)
+money = prompt("Ваш бюджет на месяц?");
+time = prompt("Введите дату в формате YYYY-MM-DD");
+
+
+// Создаем объект (п.3)
+var appData = {
+    "бюджет": money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+
+// Создаем пару переменных для (п.4) 
+var necessaryExpenses = "",
+    cost = 0;
+
+// Задаем пользователю вопросы (п.4) 
+necessaryExpenses = prompt('Введите обязательную статью расходов в этом месяце');
+cost = prompt('Во сколько обойдется?');
+
+//Записываем данные в объект
+appData.expenses[necessaryExpenses] = cost;
+
+// Повторяем еще раз
+necessaryExpenses = prompt('Введите обязательную статью расходов в этом месяце');
+cost = prompt('Во сколько обойдется?');
+
+appData.expenses[necessaryExpenses] = cost;
+
+
+//выводим на экран бюджет на 1 день (п.5)
+alert( "Ваш бюджет на день: " + appData["бюджет"] / 30 + " р.");
+
+console.log( appData );
