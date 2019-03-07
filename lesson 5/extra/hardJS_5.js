9ab84d60134277f66871784884f5bd21fd783755
filanor <filanor@gmail.com>
2019-03-08 02:48:18 +0300

    Добавлено Дополнительное задание к 5 уроку

--- lesson 5/extra/hardJS_5.js
+++ lesson 5/extra/hardJS_5.js
@@ -1,71 +1,64 @@
 'use strict'
 
-let str = "урок-3-был слишком легким";
+
+let weekArr = ['Воскресение', 'Понедельник', 'Вторник',
+        'Среда', 'Четверг', 'Пятница', 'Суббота'
+    ],
+    date = new Date(),
+    time = date.toLocaleTimeString('ru-RU'),
+    calculateBtn = document.querySelector('.date-compare__calculateBtn'),
+    dateOneInput = document.querySelector('.date-compare__date1'),
+    dateTwoInput = document.querySelector('.date-compare__date2'),
+    dateResult = document.querySelector('.date-compare__result');
 
 
-//Задание 1. 
-str = str.charAt(0).toUpperCase() + str.slice(1);
+//Напишите функцию, которая будет добавлять 0 перед днями и месяцами, которые состоят из одной цифры (из 1.6.2018 сделает 01.06.2018)
+function transformDate(date) {
+    // Функция преобразует дату к виду 01.01.2019
+    let day = date.getDate(),
+        month = date.getMonth() + 1;
 
-//Задание 2
-let startPos = 0;
-//ищем в цикле все - и заменяем на пробел
-while (true) {
-    let pos = str.indexOf('-', startPos);
-    if (pos == -1) {
-        //если не найдено совпадений - выходим из цикла
-        break;
-    } else {
-        str = str.slice(startPos, pos) + " " + str.slice(pos + 1);
-    }
+    day = (day < 10) ? ("0" + day) : day;
+    month = (month < 10) ? ("0" + month) : month;
+
+    return `${day}.${month}.${date.getFullYear()}`;
 }
-console.log(`Задание 1 и 2:  + ${str}`);
+
+//Выведите на страницу текущую дату и время в формате '09:59:59 30.05.2018'
+document.querySelector('.Date').textContent = `${time} ${transformDate(date)}`;
+
+//Напишите функцию, которая выводит на страницу текущий день недели на русском языке (реализацию выбираете сами)
+document.querySelector('.weekDay').textContent = weekArr[date.getDay()];
 
 
 
-//Задание 3
-//Вырезаем нужный участок фразы
-let str3 = str.slice(18);
 
-//а редактируем его. 
-str3 = str3.slice(0, 5) + 'оо';
-alert(str3);
+// Напишите функцию, которая выводит на страницу разницу между двумя датами в количестве дней
 
+calculateBtn.addEventListener('click', function () {
+    let reg = /^(?:[1-9]|0[1-9]|[1-2]\d|3[01])[\.\/](?:[1-9]|0[1-9]|1[0-2])[\.\/](?:[1-2]\d{3})$/;
 
-
-//Задание 4
-let arr = [20, 33, 1, "Человек", 2, 3];
-let summ = 0;
-
-for (let i = 0; i < arr.length; i++) {
-    // Исключаем не числа и находим сумму кубов
-    if (!isNaN(arr[i])) {
-        summ += arr[i] ** 3;
-    }
-}
-// выводим в консоль квадратный корень
-console.log("Задание 4: " + Math.sqrt(summ));
-
-
-
-//Задание 5
-function correctStr(str) {
-    //проверяем тип полученного аргумента
-    if (!(typeof (str) == "string")) {
-        alert("Ошибка! Получено не строчное значение!");
-        return;
+    if (reg.exec(dateOneInput.value) && reg.exec(dateTwoInput.value)) {
+        dateResult.textContent = getDifference(dateOneInput.value, dateTwoInput.value);
     } else {
-        // если все ок - убираем пробелы спереди и сзади
-        str = str.trim();
+        alert("Введены неверные данный!")
+        dateOneInput.value = '';
+        dateTwoInput.value = '';
     }
 
-    if (str.length > 50) {
-        //Если больше 50 символов, обрезаем стоку
-        console.log(`Задание 5: ${str.slice(0, 51)}...`);
-    } else {
-        console.log(`Задание 5: ${str}`);
-    }
-}
-let str5 = '   выамыва  вамав ffsadfnsadlkjcnsdlkjcnlasdjfnljkasdnflasjdnflasdjnflkasdjfnlasdjfnlsdjfn;kasdjnckajsdnc;kajdnc;kasdjnc;asdjc;sad;lc;ads  ';
+});
 
-correctStr(str5);
-correctStr(90);
+
+function getDifference(date1, date2) {
+    //Функция возвращает разницу между двумя датами. В днях
+    let arr1 = date1.split('.'),
+        arr2 = date2.split('.'),
+        rez = 0;
+
+    //создадим два объекта Date с задаными числами
+    let dateOne = new Date(arr1[2], arr1[1] - 1, arr1[0]),
+        dateTwo = new Date(arr2[2], arr2[1] - 1, arr2[0]);
+
+    // Считаем разницу между датами и переводим ее в дни
+    rez = ((+dateTwo) - (+dateOne)) / 86400 / 1000;
+    return parseInt(rez);
