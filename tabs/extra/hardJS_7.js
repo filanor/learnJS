97b61de5f01ed4b2c8eca33add4ba23249a66a16
filanor <filanor@gmail.com>
2019-03-12 01:13:48 +0300

    Правки по дополнительному заданию к 7 уроку

--- tabs/extra/hardJS_7.js
+++ tabs/extra/hardJS_7.js
@@ -2,27 +2,25 @@
 
 let circle = document.querySelector('.circle'),
     btn = document.querySelector('button'),
-    posY = circle.style.top,
-    posX = circle.style.left; 
+    posY = circle.style.top; 
     console.log(circle.style.top);
-    console.log(posX);
     let flag = true;
-btn.addEventListener('click', function(){
-    let go = setInterval(function(){
-        // circle.style.top = posY + 'px';
-        // circle.style.left = posX + 'px';
-        
-        if (posY < 450 && flag == true) {
-            posY++;
-            circle.style.top = posY + 'px';
-        } else if (posY > 0 && flag == false){
-            posY--;
-            circle.style.top = posY + 'px';
-        } else if (posY == 450){
-            flag = false;
-        }else if (posY == 0) {
-            flag = true;
-        }
-    }, 10);
 
+function anim(){
+    if (posY < 450 && flag == true) {
+        posY++;
+        circle.style.top = posY + 'px';
+    } else if (posY > 0 && flag == false){
+        posY--;
+        circle.style.top = posY + 'px';
+    } else if (posY == 450){
+        flag = false;
+    }else if (posY == 0) {
+        flag = true;
+    }
+    requestAnimationFrame(anim);
+}
+
+btn.addEventListener('click', function(){
+    anim();
 });
