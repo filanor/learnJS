17a403996eb625c6e56d920130ac7f5fc90e6855
filanor <filanor@gmail.com>
2019-03-14 02:01:02 +0300

    Доп задание к 8 уроку

--- tabs/js/script.js
+++ tabs/js/script.js
@@ -74,4 +74,56 @@ window.addEventListener('DOMContentLoaded', function () {
     }
 
     setClock('timer', deadline);
+
+
+    // smooth slide
+    let headerBox = document.querySelector('header nav ul');
+
+    headerBox.addEventListener('click', function (e) {
+
+        if (e.target && e.target.matches('li a')) {
+            e.preventDefault();
+
+            let scrolTo = e.target.getAttribute('href'),
+                // получаем элемент до которого необходимо скролить
+                scrolToBlock = document.querySelector(scrolTo),
+                //получаем расстояние до этого элемента от верхней точки окна с учетом меню (80) 
+                dirY = (scrolToBlock.getBoundingClientRect().top - 80) > 0 ? 1 : -1;
+
+            if (scrolToBlock.getBoundingClientRect().top - 80 != 0) {
+                // Если 0, то страницу крутить не нужно
+                console.log('Начинаем');
+                scroll(scrolToBlock, dirY);
+            }
+        }
+    });
+
+    function scroll(block, dirY) {
+        let duration = 1000,
+            start = Date.now(),
+            pageHeight = Math.max(
+                document.body.scrollHeight, document.documentElement.scrollHeight,
+                document.body.offsetHeight, document.documentElement.offsetHeight,
+                document.body.clientHeight, document.documentElement.clientHeight
+            );
+            
+
+        function step () {
+            let position = block.getBoundingClientRect().top - 80,
+                ste = 50,
+                now = Date.now() - start,
+                result = Math.round(position * now / duration);
+
+                result = (result > dirY * position) ? position : (result == 0) ? dirY : result;
+
+                if (dirY * position > 0 && (pageHeight - window.pageYOffset) > dirY * document.documentElement.clientHeight) {
+                    // прокручиваем страницу на величину result
+                    window.scrollBy(0,result);
+                    // рекурсивно запускаем функцию анимации прокрутки страницы
+                    requestAnimationFrame(step);
+                }
+        }
+
+    requestAnimationFrame(step);
+    }
 });
